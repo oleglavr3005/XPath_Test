a6a7b66b36e9f89f9209b4c4c2d33b54c9667ee0
oleglavr <oleglavr@ukr.net>
2016-11-17 11:37:56 +0200

    add two methods

--- src/XPath_Worker.java
+++ src/XPath_Worker.java
@@ -35,13 +35,13 @@ public class XPath_Worker {
 			XPath xpath = xpathFactory.newXPath();
 
 			String name = getAuthorNameById(doc, xpath, "myfave");
-			System.out.println("Employee Name with ID 4: " + name);
+			System.out.println("Author of book with id \"fave\": " + name);
 
-			List<String> names = getEmployeeNameWithAge(doc, xpath, 30);
-			System.out.println("Employees with 'age>30' are:" + Arrays.toString(names.toArray()));
+			List<String> names = getBooksPricesByStyle(doc, xpath, "autobiography");
+			System.out.println("Prices of books with style 'autobiography' are:" + Arrays.toString(names.toArray()));
 
-			List<String> femaleEmps = getFemaleEmployeesName(doc, xpath);
-			System.out.println("Female Employees names are:" + Arrays.toString(femaleEmps.toArray()));
+			List<String> femaleEmps = getMagazineStyles(doc, xpath);
+			System.out.println("Styles of magazines:" + Arrays.toString(femaleEmps.toArray()));
 
 		} catch (ParserConfigurationException | SAXException | IOException e) {
 			e.printStackTrace();
@@ -49,11 +49,11 @@ public class XPath_Worker {
 
 	}
 
-	private static List<String> getFemaleEmployeesName(Document doc, XPath xpath) {
+	private static List<String> getMagazineStyles(Document doc, XPath xpath) {
 		List<String> list = new ArrayList<>();
 		try {
 			// create XPathExpression object
-			XPathExpression expr = xpath.compile("/Employees/Employee[gender='Female']/name/text()");
+			XPathExpression expr = xpath.compile("//magazine/@style");
 			// evaluate expression result on XML document
 			NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
 			for (int i = 0; i < nodes.getLength(); i++)
@@ -64,11 +64,11 @@ public class XPath_Worker {
 		return list;
 	}
 
-	private static List<String> getEmployeeNameWithAge(Document doc, XPath xpath, int age) {
+	private static List<String> getBooksPricesByStyle(Document doc, XPath xpath, String style) {
 		List<String> list = new ArrayList<>();
 		try {
-			XPathExpression expr = xpath.compile("/Employees/Employee[age>" + age + "]/name/text()");
-			NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+			XPathExpression expr0 = xpath.compile("//book[@style=\"" + style + "\"]/price/text()");
+			NodeList nodes = (NodeList) (expr0.evaluate(doc, XPathConstants.NODESET));
 			for (int i = 0; i < nodes.getLength(); i++)
 				list.add(nodes.item(i).getNodeValue());
 		} catch (XPathExpressionException e) {
